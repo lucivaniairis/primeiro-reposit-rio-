9c868e9aa0de311d870fbceeea8a8f16626b73b0
lucivaniairis <97631031+lucivaniairis@users.noreply.github.com>
2022-01-13 15:12:40 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,9 @@
+nome=str('IRIS')
+print(nome[:4:2])
+print(nome[::-1])
+lista=list(range(10))
+i=0
+while i <len(nome):
+    print(nome[i])
+    i+=1
+    
---
+++ tipos primitivos.py
@@ -0,0 +1,6 @@
+n=float(input('digite um numero:'))
+n1=float(input('digite outro número:'))
+media=(n1+n)/2
+print('A média de {:.0f} e {:.0f} é {:.0f}'.format(n, n1, media))
+
+

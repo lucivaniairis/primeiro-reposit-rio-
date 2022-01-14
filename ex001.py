98723e8b18c7a3802b12b3c2f13f29ab9ccb9076
lucivaniairis <97631031+lucivaniairis@users.noreply.github.com>
2022-01-14 15:26:21 -0300

    Add files via upload

---
+++ ex001.py
@@ -0,0 +1,3 @@
+print('Olá Mundo!')
+nome=str(input('Qual seu nome?')).upper()
+print('Olá {} Seja Bem Vindo!'.format(nome))
---
+++ ex005.py
@@ -0,0 +1,7 @@
+num=int(input('Digite um número:'))
+ante=num-1
+suce=num+1
+print('O número {} tem como Antecessor {} e sucessor {}'.format(num, ante, suce))
+#também poderia ser feito dessa maneira:
+#n=int(input('Digite um número:'))
+#print('O número {} tem comno antecessor {} e sucessor{}'.format(n,(n-1),(n+1))

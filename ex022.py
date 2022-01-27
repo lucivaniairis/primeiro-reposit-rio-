83ab21aaf7c5aaf365cd53cf87f8b82de1246383
Lucivania Iris <97631031+lucivaniairis@users.noreply.github.com>
2022-01-27 17:52:17 -0300

    Add files via upload

---
+++ ex022.py
@@ -0,0 +1,6 @@
+nome=str(input('Digite o nome completo:')).strip()
+print('Analisando seu nome...')
+print('Seu nome em maiúsculas é {}'.format(nome.upper()))
+print('Seu nome em minúsculas é {}'.format(nome.lower()))
+print('Seu nome ao todo tem {} letras'.format(len(nome)-nome.count(' ')))
+print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
---
+++ ex025.py
@@ -0,0 +1,2 @@
+nome=str(input('Digite seu nome:')).strip().upper()
+print('Seu nome tem silva? {}'.format('SILVA' in nome))

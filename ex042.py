25cf9dcc0eaa0d00ac52632787330a63ddb3ba98
Lucivania Iris <97631031+lucivaniairis@users.noreply.github.com>
2022-02-04 18:17:58 -0300

    Add files via upload

--- ex042.py
+++ ex042.py
@@ -7,7 +7,7 @@ if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
         print('EQUILÁTERO!')
     elif r1!=r2!=r3!=r1:
         print('ESCALENO')
-    elif r1==r2!=r3 and r2==r3!=r1 and r1==r3!=r2:
+    else:
         print('ISÓSCELES')
 else:
     print('Os segmentos acima NÃO PODEM FORMAR triângulo')

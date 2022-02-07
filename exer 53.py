508f3034f1fdf30f9bf721f572f6a8cde1526468
Lucivania Iris <97631031+lucivaniairis@users.noreply.github.com>
2022-02-07 17:50:58 -0300

    Delete exer 53.py

--- exer 53.py
+++
@@ -1,9 +0,0 @@
-frase=str(input('Digite uma frase:')).strip().upper()
-palavras=frase.split()
-junto=''.join(palavras)
-inverso=junto[::-1]
-print('O inveso de {} é {}'.format(junto, inverso))
-if inverso==junto:
-    print('Temos um PALÍNDROMO!')
-else:
-    print('A frase digitada não é um PALÍNDROMO!')

a38115c78637c3869af080bf0546cfb265b97115
Lucivania Iris <97631031+lucivaniairis@users.noreply.github.com>
2022-02-07 17:50:05 -0300

    Delete exer 51.py

--- exer 51.py
+++
@@ -1,6 +0,0 @@
-primeiro=int(input('Primeiro termo:'))
-razao=int(input('Razão:'))
-decimo=primeiro+(10-1)*razao
-for c in range(primeiro,decimo+razao,razao):
-    print('{}'.format(c), end='->')
-print('ACABOU')
